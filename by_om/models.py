92539bde5b9026737d674b6d21c359b881e1c383
Caleb <CGibney@users.noreply.github.com>
2019-03-24 15:38:46 -0400

    Update models.py

--- by_om/models.py
+++ by_om/models.py
@@ -3,14 +3,15 @@ from django.db import models
 class Plants(models.Model):
     description = models.CharField(max_length=200)
     environment = models.ManyToMany(Environment, on_delete=models.CASCADE)
-    plantType   = models.
+    #plantType   = models.
     
     
 class Environment(models.Model):
     description = models.CharField(max_length=200)
     # unnecessary per official documentation; only needed in one model
     # plant = models.ManyToManyField(Plants, on_delete=models.CASCADE)
-    environmentType = 
-    temperate
-    temp_start
-    temp_end
+    
+    #environmentType = 
+    #temperate
+    #temp_start
+    #temp_end
