0f3b7ee9cd3d9dede5c6a88666a5a42cb5eeb6e2
Caleb <CGibney@users.noreply.github.com>
2019-04-27 11:06:25 -0400

    Fix problem with models.py

--- byom_cbv/models.py
+++ byom_cbv/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 class Biome(models.Model):
     BIOME_NAME = models.CharField(max_length=200)
     SOIL_PH_MIN = models.FloatField()
-    SOIL_PH_MAX = models.FloatField()	
+    SOIL_PH_MAX = models.FloatField()   
     TEMP_MIN = models.FloatField()
     TEMP_MAX = models.FloatField()
     PRECIP_MIN = models.FloatField()
@@ -25,10 +25,11 @@ class User(models.Model):
     # No authentication should be used for now - we shouldn't "roll our own" solution.
 
 
+    # fixed indentation issue
  class Bookmark(models.Model):
-       BOOKMARK_DATE = models.DateField()
-       crop = models.ForeignKey(Crop, on_delete=models.CASCADE)
-       user = models.ForeignKey(User, on_delete=models.CASCADE)
+   BOOKMARK_DATE = models.DateField()
+   crop = models.ForeignKey(Crop, on_delete=models.CASCADE)
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def __str__(self):
